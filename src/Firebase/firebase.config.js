2bebd9022d9abf72e0298c729ade50e910881fa6
alamin500 <nalamin500@gmail.com>
2021-10-08 09:46:06 +0600

    h

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7PUHglVotNcsfEoxREz3HSIRDGBsPhl0",
+  authDomain: "react-firebase-authentic-8deb7.firebaseapp.com",
+  projectId: "react-firebase-authentic-8deb7",
+  storageBucket: "react-firebase-authentic-8deb7.appspot.com",
+  messagingSenderId: "115403416848",
+  appId: "1:115403416848:web:1832b9641cd080e2777195",
+};
+export default firebaseConfig;
